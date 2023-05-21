42670adab7e3344fe847fe0c4e71e939a95f1d86
AnushkaChaudhary1322 <122356104+AnushkaChaudhary1322@users.noreply.github.com>
2023-05-21 17:10:59 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,4 +1,13 @@
-
+const questions = [
+    {
+        question: "What is the highest grossing Bollywood studio of all time?",
+        answers: [
+            {text: "Dharma Productions", correct: true},
+            {text: "Yash Raj Films", correct: false},
+            {text: "Fox Star Studios", correct: false},
+            {text: "T-Series", correct: false},
+        ]
+    },
     {
         question: "Who plays the female lead in the movie 'Chandni Chowk to China'?",
         answers: [
